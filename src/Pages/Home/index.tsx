71b3b96f7cae74f327c8caa0de0f32d64d6fb995
Crhistian de Oliveira <crhistiandeoliveira@gmail.com>
2022-06-20 23:51:37 -0300

    finaliza atividade

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,10 +1,35 @@
 import CardSuperTrunfo from "../../components/CardSuperTrunfo"
+import Wrapper from "../../components/Wrapper"
+
 
 function Home() {
     
     return (
         <>
-            <CardSuperTrunfo/>
+            <Wrapper align="center" justify="center" direction="row" height="100vh" style={{gap: '24px'}}>
+
+                <CardSuperTrunfo 
+                nome='Fusca Azul' 
+                topTag="3A" 
+                imageLink="https://tudosobreprodutos.com.br/img/miniatura-metal-carro-antigo-colecao-fusca-azul.png" 
+                velocidade={120}
+                cilindradas={1200}
+                zeroAcem={40}
+                potencia={2}
+                />
+
+                <CardSuperTrunfo 
+                nome='Camaro' 
+                topTag="3B" 
+                imageLink="https://www.agoramotor.com.br/wp-content/uploads/2021/09/Chevrolet-Camaro-2022-destaque.jpg" 
+                velocidade={300}
+                cilindradas={5000}
+                zeroAcem={4}
+                potencia={200}
+                />
+
+            </Wrapper>
+
         </>
     )
 }
--- src/components/CardSuperTrunfo/index.tsx
+++ src/components/CardSuperTrunfo/index.tsx
@@ -27,15 +27,140 @@ const InnerCard = styled(OuterCard)`
     font-weight: bold;
 `
 
-export default function CardSuperTrunfo() {
+const CardFooter = styled.p`
+text-align: center;
+font-weight: bold;
+color:  ${props=>props.theme.pallet.text};
+`
+const LogoFooter = styled.img.attrs({
+src: 'https://www.growdev.com.br/assets/images/logo-dark.png',
+alt: "logo",
+})`
+    height: 40px;
+    object-fit: cover;
+    margin-top: -2px;
+  `
+
+const TopLetterWrapper = styled.div`
+    border-radius: 50%;
+    border: 4px solid white;
+    height: 38px;
+    width: 38px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: -12px;
+    position: absolute;
+`
+const TopLetter= styled.div`
+    border-radius: 50%;
+    border: 4px solid  ${props=>props.theme.pallet.secondary};
+    background-color:  ${props=>props.theme.pallet.background};
+    color:  ${props=>props.theme.pallet.text};
+    height: 32px;
+    width: 32px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: -2px;
+`
+
+const Imagem = styled.img`
+width: 250px ;
+height: 150px ;
+object-fit: cover;
+border-radius: 48px;
+`
+
+const Title = styled.p`
+text-align: center;
+font-weight: bold;
+font-size: large;
+width: 100%;
+border-radius: 8px;
+padding: 4px 0px;
+background-color:  ${props=>props.theme.pallet.secondary};
+color:  ${props=>props.theme.pallet.text};
+
+`
+
+const DivAtributos = styled.div<{inner?:boolean, even?: boolean}>`
+display: flex;
+align-items: center;
+width: ${props=>props.inner ? 'unset' : '100%'} ;
+justify-content: ${props=>props.inner ? 'center' : 'space-between'} ;
+background-color: ${props=>props.even ? props.theme.pallet.secondary : props.theme.pallet.background} ;
+border-radius: 8px;
+padding: 0px 2px;
+height: 24px;
+`
+
+const Icon = styled.img`
+height: 20px;
+margin-right: 4px;
+`
+
+const AtributoNome = styled.p`
+font-size: small;
+`
+const AtributoValor = styled.p`
+font-size: small;
+`
+
+type Car = {
+    nome: string
+    topTag: string
+    imageLink: string
+    velocidade: number
+    cilindradas: number
+    zeroAcem: number
+    potencia: number
+}
+export default function CardSuperTrunfo(props:Car) {
     
     return (
-        <Wrapper align="center" justify="center" direction="column" height="100vh">
             <OuterCard>
                 <InnerCard>
-                    teste
+                    <TopLetterWrapper>
+                        <TopLetter>3A</TopLetter>
+                    </TopLetterWrapper>
+                    <Wrapper style={{borderRadius: '48px'}} align="center" justify="space-between" direction="column" height="100%">
+                        <Imagem src={props.imageLink}/>
+                        <Title>{props.nome}</Title>
+                        <DivAtributos>
+                            <DivAtributos inner>
+                                <Icon src='https://img.icons8.com/fluency-systems-filled/452/chart-bar.png'></Icon>
+                                <AtributoNome>Velocidade máxima</AtributoNome>
+                            </DivAtributos>
+                            <AtributoValor>{props.velocidade} KM/H</AtributoValor>
+                        </DivAtributos>
+                        <DivAtributos even>
+                            <DivAtributos inner even >
+                                <Icon src='https://img.icons8.com/metro/452/scuba-pressure-gauge.png'></Icon>
+                                <AtributoNome>Cilindradas</AtributoNome>
+                            </DivAtributos>
+                            <AtributoValor>{props.cilindradas} CC</AtributoValor>
+                        </DivAtributos>
+                        <DivAtributos>
+                            <DivAtributos inner>
+                                <Icon src='https://img.icons8.com/material-rounded/344/speedometer.png'></Icon>
+                                <AtributoNome>0 à 100</AtributoNome>
+                            </DivAtributos>
+                            <AtributoValor>{props.zeroAcem} S</AtributoValor>
+                        </DivAtributos>
+                        <DivAtributos even>
+                            <DivAtributos inner even >
+                                <Icon src='https://img.icons8.com/external-kmg-design-glyph-kmg-design/344/external-thunder-weather-kmg-design-glyph-kmg-design.png'></Icon>
+                                <AtributoNome>Potência</AtributoNome>
+                            </DivAtributos>
+                            <AtributoValor>{props.potencia} CV</AtributoValor>
+                        </DivAtributos>
+                        <div style={{display: 'flex', marginTop: '8px'}}>
+                            <CardFooter>Super Trunfo </CardFooter>
+                            <LogoFooter/>
+                        </div>
+                    </Wrapper>
                 </InnerCard>
             </OuterCard>
-        </Wrapper>
     )
 }
